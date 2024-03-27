911b64d1bc250ac453ebe9a550253174b417ee5f
erioluwa66 <erioluwa66@gmail.com>
2024-03-27 19:40:17 -0400

    footer adjusted to fit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,37 +3,58 @@ import busytoddlermumlogo from "../../assets/logos/YummytoddlerMum.svg";
 import facebook from "../../assets/icons/Icon-facebook.png";
 import instagram from "../../assets/icons/Icon-instagram.png";
 import twitter from "../../assets/icons/Icon-twitter.png";
+import Linkedin from "../../assets/icons/icon-linkedin.png"
 import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <div className="Footer">
-      <div>
+    <div className="footer">
+      <div className="footer__top">
         <Link to="/">
           <img
-            className=""
+            className="footer__logo"
             src={busytoddlermumlogo}
             alt="busytoddlermum-logo"
           />
         </Link>
-        <p>
+        <h3 className="footer__text">
           Discover a treasure trove of toddler-approved meals, designed to be
           nutritious and fun.
-        </p>
-        <button>CONTACT US</button>
+        </h3>
       </div>
-      <div>
-        <Link to="https://www.instagram.com/">
-          <img className="footer-icon" src={instagram} alt="instagram" />
-        </Link>
-        <Link to="https://www.facebook.com/">
-          <img className="footer-icon" src={facebook} alt="facebook" />
-        </Link>
-        <Link to="https://www.twitter.com/">
-          <img className="footer-icon" src={twitter} alt="twitter" />
-        </Link>
+      <div className="footer__bottom">
+        <div className="footer__social">
+          <Link to="https://www.instagram.com/">
+            <img
+              className="footer__social--icon"
+              src={instagram}
+              alt="instagram"
+            />
+          </Link>
+          <Link to="https://www.facebook.com/">
+            <img
+              className="footer__social--icon"
+              src={facebook}
+              alt="facebook"
+            />
+          </Link>
+          <Link to="https://www.twitter.com/">
+            <img className="footer__social--icon" src={twitter} alt="twitter" />
+          </Link>
+          <Link to="https://www.linkedin.com/">
+            <img
+              className="footer__social--icon"
+              src={Linkedin}
+              alt="linkedin"
+            />
+          </Link>
+        </div>
+        <div>
+          <h3 class="footer__copyright">
+            © 2024 Busy Toddler Mum All Rights Reserved
+          </h3>
+        </div>
       </div>
-      <h3 class="footer__copyright">© 2024 Busy Toddler Mum All Rights Reserved</h3>
     </div>
   );
 }
