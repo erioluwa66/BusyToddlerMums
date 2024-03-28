3a6abb856314fe978b02147ad9d4a0b7d6781e6d
erioluwa66 <erioluwa66@gmail.com>
2024-03-28 00:20:52 -0400

    home details added

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/meal-plan" element={<MealPlan />} />
           <Route path="/recipes" element={<Recipes />} />
+          <Route path="/recipes/:id" element={<RecipesPage />} />
         </Routes>
         <Footer />
       </BrowserRouter>
