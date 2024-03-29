b499fb5fa542f8d5f99d4f38015b9fcd6e7e8ba1
erioluwa66 <erioluwa66@gmail.com>
2024-03-29 15:27:17 -0400

    hero logo placed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,17 @@
-import heroIllustration from "../../assets/icons/Hero_Illustration.svg"
+import heroIllustration from "../../assets/icons/Hero_Illustration.svg";
 // import backgroundframe from "../../assets/icons/BackgroundFrame.svg"
-import bluecupcake from "../../assets/icons/Frame_blue.svg"
+import { useEffect, useState } from "react";
+import axios from "axios";
+import bluecupcake from "../../assets/icons/Frame_blue.svg";
 // import pinkcupcake from "../../assets/icons/Frame_pink.svg"
 import { Link } from "react-router-dom";
-import "./Home.scss"
+import "./Home.scss";
 
 function Home() {
+  const [popularRecipes, setPopularRecipes] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const baseUrl = "http://";
+
   return (
     <div className="hero">
       {/* <img className="hero_icon" src={pinkcupcake} alt={pinkcupcake} /> */}
@@ -19,26 +25,27 @@ function Home() {
           <p className="hero__main-text--sub">
             Mia, a whirlwind mom juggling work and rambunctious twins, dreaded
             mealtimes, BusyToddlerMum, a web app became her hero. This app
-            offered delcious, toddler, approved recipes, a magic filter to find
+            offered delicious, toddler, approved recipes, a magic filter to find
             meals based on what's already in the fridge, and personalized meal
             plans for stress-free prep. With BusyToddlerMum, Mia saved time,
             enjoyed cooked again, and created happy mealtime memories with her
             family. Let BusyToddlerMum be your hero too!
           </p>
           <Link to="/recipes">
-            <button className="">Get a Recipe</button>
+            <button className="hero__btn">Get a Recipe</button>
           </Link>
         </div>
         <img
-          className="hero_logo"
+          className="hero__logo"
           src={heroIllustration}
           alt={heroIllustration}
         />
       </div>
-      <img className="hero_icon" src={bluecupcake} alt={bluecupcake} />
+
       <div className="hero__bottom">
         <p>Popular Recipes</p>
       </div>
+      <img className="hero_icon" src={bluecupcake} alt={bluecupcake} />
     </div>
   );
 }
