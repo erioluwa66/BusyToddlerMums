b15729246b435545953d1795554dfde316ed111d
erioluwa66 <erioluwa66@gmail.com>
2024-03-29 19:35:09 -0400

    filter succesfully added

--- src/components/RecipeList/RecipeList.jsx
+++ src/components/RecipeList/RecipeList.jsx
@@ -1,19 +1,33 @@
-// components/RecipeList.js
-import "./RecipeList.scss"; // Import CSS file for styling
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-
+import "./RecipeList.scss";
 
 function RecipeList() {
   const [recipes, setRecipes] = useState([]);
+  const [allRecipes, setAllRecipes] = useState([]); // Store all recipes for resetting filters
+  const [cuisines, setCuisines] = useState([]);
+  const [courses, setCourses] = useState([]);
+  const [selectedCuisine, setSelectedCuisine] = useState("");
+  const [selectedCourse, setSelectedCourse] = useState("");
   const navigate = useNavigate();
+  const baseUrl = "http://localhost:5050";
 
   useEffect(() => {
     const fetchRecipes = async () => {
       try {
-        const response = await axios.get("http://localhost:5050/api/recipes"); // Assuming your backend server is running on the same host
+        const response = await axios.get(`${baseUrl}/api/recipes`);
+        setAllRecipes(response.data); // Store all recipes
         setRecipes(response.data);
+
+        const uniqueCuisines = [
+          ...new Set(response.data.map((recipe) => recipe.cuisine)),
+        ];
+        const uniqueCourses = [
+          ...new Set(response.data.map((recipe) => recipe.course)),
+        ];
+        setCuisines(uniqueCuisines);
+        setCourses(uniqueCourses);
       } catch (error) {
         console.error("Error fetching recipes:", error);
       }
@@ -21,21 +35,81 @@ function RecipeList() {
     fetchRecipes();
   }, []);
 
-  //Function to handle click event on recipe card
+  useEffect(() => {
+    // Filter recipes based on selected cuisine and course
+    const filteredRecipes = allRecipes.filter((recipe) => {
+      return (
+        (selectedCuisine ? recipe.cuisine === selectedCuisine : true) &&
+        (selectedCourse ? recipe.course === selectedCourse : true)
+      );
+    });
+    setRecipes(filteredRecipes);
+  }, [selectedCuisine, selectedCourse, allRecipes]);
+
   const handleRecipeClick = (id) => {
-    navigate(`/recipes/${id}`); // Navigate to the recipe details
+    navigate(`/recipes/${id}`);
+  };
+  // Handler for course checkbox change
+  const handleCuisineChange = (course) => {
+    setSelectedCuisine(course === selectedCourse ? '' : course);
   };
 
+  //handler for cuisine checkbox change
+  const handleCourseChange = (course) => {
+    setSelectedCourse(course === selectedCourse ? '' : course);
+  }
+
   return (
-    <div className="recipe-list">
-      {recipes.map((recipe) => (
-        <div className="recipe-card" key={recipe.id} onClick={() => handleRecipeClick(recipe.id)}>
-          <img src={recipe.image} alt={recipe.recipe_name} />
-          <h3>{recipe.recipe_name}</h3>
-          <p>{recipe.description}</p>
-          {/* Add other recipe details as needed */}
-        </div>
-      ))}
+    <div className="content-container">
+      <div className="filters">
+        <h2>Courses</h2>
+        <ul>
+          {courses.map((course, index) => (
+            <li
+              key={index}
+              onClick={() =>
+                handleCourseChange(course)}>
+              <input 
+              type="checkbox"
+              checked={selectedCourse === course}
+              onChange={() => {}}
+              onClick={(e) => e.stopPropagation()} // Prevent li from triggering when li is clicked 
+              /> 
+              {course}
+            </li>
+          ))}
+        </ul>
+        <h2>Cuisines</h2>
+        <ul>
+          {cuisines.map((cuisine, index) => (
+            <li
+              key={index}
+              onClick={() =>
+                handleCuisineChange(cuisine)}>
+                  <input 
+                  type="checkbox"
+                  checked={selectedCuisine === cuisine}
+                  onChange={() => {}}
+                  onClick={(e) => e.stopPropagation()}
+                  />
+              {cuisine}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="recipe-list">
+        {recipes.map((recipe) => (
+          <div
+            className="recipe-card"
+            key={recipe.id}
+            onClick={() => handleRecipeClick(recipe.id)}
+          >
+            <img src={recipe.image} alt={recipe.recipe_name} />
+            <h3>{recipe.recipe_name}</h3>
+            <p>{recipe.description}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
